ed6a652191bc953e1ec45b6f72d28ad8b7f09fdb
hewydewyle <le.hieu@gmail.com>
2015-02-11 02:37:00 +0000

    quotes form to modal

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,10 +2,6 @@ class QuotesController < ApplicationController
 	def index
 		@quote = Quote.order("RANDOM()").first
 	end
-	
-	def new
-		@quote = Quote.new
-	end
 
 	def create
 		@quote = Quote.create(quote_params)
@@ -14,7 +10,7 @@ class QuotesController < ApplicationController
 		end
 		redirect_to root_path
 	end
-	
+
 	def about
 	end
 
